3d8f2534076b080a87154de3c1c8f7e017b8e95b
Thomas Plumpton <Thomas.Plumpton@hotmail.co.uk>
2018-11-14 20:22:58 +0000

    Updated Service JavaDoc

--- src/main/java/com/domain/Service.java
+++ src/main/java/com/domain/Service.java
@@ -1,4 +1,18 @@
 package com.domain;
 
+/**
+ * Sometimes, it just isn't a thing. In some cases, the clearest and most pragmatic design includes operations that do
+ * not conceptually belong to any object. Rather than force the issues, we can follow the natural contours of the
+ * problem space and include **Services** explicitly in the model.
+ *
+ * A good **Service** has three characteristics;
+ * 1. The operation relates to a domain concept that is not a natural part of an **Entity** or **Value Object**.
+ * 2. The interface is defined in terms of other elements of the domain model.
+ * 3. The operation is stateless.
+ *
+ * @author Thomas Plumpton
+ * @see Entity
+ * @see ValueObject
+ */
 public interface Service {
 }
