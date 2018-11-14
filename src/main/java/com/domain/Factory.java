b93db2ddd6df163b56bb373b9162ca5d3f4d2bad
Thomas Plumpton <Thomas.Plumpton@hotmail.co.uk>
2018-11-14 20:14:57 +0000

    Updated Factory JavaDoc

--- src/main/java/com/domain/Factory.java
+++ src/main/java/com/domain/Factory.java
@@ -1,7 +1,15 @@
 package com.domain;
 
 /**
- * A Domain-Driven Design Factory.
+ * When the creation of an object, or an entire **Aggregate**, becomes complicated or reveals too much of the internal
+ * structure, **Factories** provide encapsulation.
+ *
+ * Creation of an object can be a major operation in itself, but complex assembly operations do not fit the
+ * responsibility of the created objects. Combining such responsibilities can produce ungainly designs that are hard
+ * to understand. Making the client direct construction muddies the design of the client, breaches encapsulation of
+ * the assembled object or **Aggregate**, and overly couples the client to the implementation of the created object.
+ *
+ * @author Thomas Plumpton
  */
 public interface Factory {
 }
