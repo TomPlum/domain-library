4b63a0712927cf70c22a27e6a43e8fd6f0d19a77
Thomas Plumpton <Thomas.Plumpton@hotmail.co.uk>
2018-11-14 20:08:34 +0000

    Updated Aggregate JavaDoc

--- src/main/java/com/domain/Aggregate.java
+++ src/main/java/com/domain/Aggregate.java
@@ -1,8 +1,17 @@
 package com.domain;
 
 /**
- * A Domain-Driven Design Aggregate Root
- * @param <T> Value Type
+ * An **Aggregate** is a cluster of associated objects that we treat as a unit for the purpose of data changes.
+ *
+ * Each **Aggregate** has a root and a boundary. The boundary defines what is inside the **Aggregate**.
+ *
+ * The root is a single, specific **Entity** contained in the **Aggregate**.
+ * 
+ * The root is the only member of the **Aggregate** that outside objects are allowed to hold references to,
+ * although objects within the boundary may hold references to each other.
+ *
+ * @author Thomas Plumpton
+ * @param <T> Value (Generic Type)
  * @see Entity
  */
 public interface Aggregate<T> extends Entity<T> {
