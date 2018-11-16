fc0efbdb61653d6b1045152b26d55fc435a6d41e
Thomas Plumpton <Thomas.Plumpton@hotmail.co.uk>
2018-11-16 22:56:53 +0000

    Amended JavaDoc Links

--- src/main/java/com/domain/Repository.java
+++ src/main/java/com/domain/Repository.java
@@ -4,7 +4,7 @@ package com.domain;
  * Responsible for CRUD (Create, Read, Update Delete) operations of Aggregates that have uniquely identifying key values.
  *
  * Associations allow us to find an object based on its relationship to another. But we must have a starting point for
- * a traversal of an {@link Entity} or **Value** in the middle of its life-cycle.
+ * a traversal of an {@link Entity} or {@link ValueObject} in the middle of its life-cycle.
  *
  * For each type of object that needs global access, create an object that can provide the illusion of an in-memory
  * collection of all objects of that type. Set up access through a well-known global interface. Provide methods to add
@@ -20,6 +20,8 @@ package com.domain;
  * @param <K> Key
  * @param <A> Value (Aggregate)
  * @see Aggregate
+ * @see Entity
+ * @see ValueObject
  */
 public interface Repository<K, A extends Aggregate<?>> {
     /**
--- src/main/java/com/domain/Service.java
+++ src/main/java/com/domain/Service.java
@@ -5,7 +5,7 @@ package com.domain;
  * not conceptually belong to any object. Rather than force the issues, we can follow the natural contours of the
  * problem space and include {@link Service} explicitly in the model.
  *
- * A good **Service** has three characteristics;
+ * A good service has three characteristics;
  * 1. The operation relates to a domain concept that is not a natural part of an {@link Entity} or {@link ValueObject}.
  * 2. The interface is defined in terms of other elements of the domain model.
  * 3. The operation is stateless.
